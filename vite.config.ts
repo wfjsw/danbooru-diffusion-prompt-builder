e6ea995942e81fece572592bdd9b643ec432cff7
wfjsw <wfjsw@users.noreply.github.com>
2022-10-14 08:35:06 +0800

    update new domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import yaml from "@rollup/plugin-yaml"
 import ElementPlus from 'unplugin-element-plus/vite'
+import Radar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), yaml(), ElementPlus( { useSource: true } )],
-  resolve: {
-    alias: {
-        '@': '/src',
+    plugins: [
+        vue(),
+        yaml(),
+        ElementPlus( { useSource: true } ),
+        {
+            ...Radar({
+                analytics: {
+                    id: 'G-WYETBX79HJ',
+                }
+            }),
+            apply(config, {command}) {
+                return command === 'build' && config.mode === 'production'
+            }
+        }
+    ],
+    resolve: {
+        alias: {
+            '@': '/src',
+        }
     }
-  }
 })
