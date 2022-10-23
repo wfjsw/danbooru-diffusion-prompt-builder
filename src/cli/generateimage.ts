abb1732a7926ba135e181342c27d300df79fe853
wfjsw <wfjsw@users.noreply.github.com>
2022-10-23 11:03:58 +0800

    add photos

--- src/cli/generateimage.ts
+++ src/cli/generateimage.ts
@@ -11,9 +11,14 @@ const root = path.resolve(dirname, '../../')
 const tagFiles = glob.sync('**/*.yaml', {cwd: path.resolve(dirname, '../../data/tags')})
 const tagSet: Set<string> = new Set()
 
+const uc = 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry'
+const ucA = uc.split(', ')
+
 for (const file of tagFiles) {
+    if (file.startsWith('restricted')) continue
     const tagData: TagCategories = yaml.load(fs.readFileSync(path.resolve(dirname, '../../data/tags', file), 'utf-8')) as TagCategories
     for (const [tag, meta] of Object.entries(tagData.content)) {
+        if (ucA.includes(tag)) continue
         if (!meta.image) {
             tagSet.add(tag)
         }
@@ -31,7 +36,7 @@ for (const tag of tagSet) {
 
     const params = JSON.stringify({
         prompt: `masterpiece, best quality, ${tag}`,
-        negative_prompt: 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry',
+        negative_prompt: uc,
         steps: 28,
         cfg_scale: 12,
         do_not_save_samples: true,
@@ -39,7 +44,7 @@ for (const tag of tagSet) {
         height: 512,
         clip_skip: 2,
         sampler_index: 'Euler a',
-        sd_model: 'model.pruned.ckpt [925997e9]',
+        sd_model: 'modelsfw.pruned.ckpt [1d4a34af]',
     })
 
     const res = await fetch('http://127.0.0.1:7860/sdapi/v1/txt2img', {
